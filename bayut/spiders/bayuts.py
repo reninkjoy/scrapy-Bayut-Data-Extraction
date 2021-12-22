e151cb6daa6ca310453ed94d151ebdf6b54d1205
reninkjoy <44253152+reninkjoy@users.noreply.github.com>
2021-12-22 13:12:12 +0530

    Update bayuts.py
    fetch link datas

--- bayut/spiders/bayuts.py
+++ bayut/spiders/bayuts.py
@@ -8,11 +8,13 @@ class BayutsSpider(scrapy.Spider):
 
     def parse(self, response):
         for link in response.css('div._4041eb80'):
-            yied{"link":link.css('a._287661cb').attrib['href']}
-            
-            
+            yield response.follow(pages,callback=self.parse_data)
+                
         for link in response.css('a.b7880daf'):
             if link.css("a.b7880daf").attrib['title']=="Next":
                 next_page=link.css('a.b7880daf').attrib['href']
         if next_page is not None:
             yield response.follow(next_page,callback=self.parse,dont_filter=True)
+            
+    def parse_data(self,response):
+        yield{"data":response.css("div._1f0f1758::text").getall()}
