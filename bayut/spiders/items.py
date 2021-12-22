8b6981d1c4948e8f42a06e526710b17e9757acd3
reninkjoy <44253152+reninkjoy@users.noreply.github.com>
2021-12-22 19:07:52 +0530

    Create items.py

---
+++ bayut/spiders/items.py
@@ -0,0 +1,16 @@
+import scrapy
+class Author(scrapy.Item):
+    property_id = scrapy.Field()
+    purpose = scrapy.Field()
+    type=scrapy.Field()
+    added_on=scrapy.Field()
+    furnishing=scrapy.Field()
+    price=scrapy.Field()
+    location=scrapy.Field()
+    bed_bath_size=scrapy.Field()
+    permit_number=scrapy.Field()
+    agent_name=scrapy.Field()
+    image_url=scrapy.Field()
+    breadcrumbs=scrapy.Field()
+    amenities=scrapy.Field()
+    description=scrapy.Field()
