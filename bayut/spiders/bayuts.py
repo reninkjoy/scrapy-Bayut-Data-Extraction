3454dcbb41377642ea1c49af546f0a649186013f
reninkjoy <44253152+reninkjoy@users.noreply.github.com>
2021-12-23 00:59:36 +0530

    Update bayuts.py

--- bayut/spiders/bayuts.py
+++ bayut/spiders/bayuts.py
@@ -1,4 +1,4 @@
-import scrapy
+iimport scrapy
 from .items import Author
 
 class DatasSpider(scrapy.Spider):
@@ -7,16 +7,18 @@ class DatasSpider(scrapy.Spider):
     start_urls = ['https://www.bayut.com/to-rent/property/dubai/']
     
     def parse(self, response):
+
         for link in response.css('div._4041eb80'):
             pages = link.css('a._287661cb').attrib['href']
             yield response.follow(pages,callback=self.parse_data)
-#       link of pages
-#         for link in response.css('a.b7880daf'):
-#             if link.css("a.b7880daf").attrib['title']=="Next":
-#                 next_page=link.css('a.b7880daf').attrib['href']
-# #       go to next pages
-#         if next_page is not None:
-#             yield response.follow(next_page,callback=self.parse,dont_filter=True)
+
+        for link in response.css('a.b7880daf'):
+            if link.css("a.b7880daf").attrib['title']=="Next":
+                next_page=link.css('a.b7880daf').attrib['href']
+
+#       go to next pages
+        if next_page is not None:
+            yield response.follow(next_page,callback=self.parse,dont_filter=True)
 
     def parse_data(self,response):
 #       setting order load in to json
@@ -37,27 +39,28 @@ class DatasSpider(scrapy.Spider):
         author['description']=None
 
 # #      fetch data
-        author['property_id']=response.xpath("//span[@aria-label='Reference']/text()").extract()
-        author['purpose']=response.xpath("//span[@aria-label='Reference']/text()").extract()
-        author['type']=response.xpath("//span[@aria-label='Type']/text()").extract()
-        author['added_on']=response.xpath("//span[@aria-label='Reactivated date']/text()").extract()
-        author["furnishing"]=response.xpath("//span[@aria-label='Furnishing']/text()").extract()
+        author['property_id']=response.xpath("//span[@aria-label='Reference']/text()").get()
+        author['purpose']=response.xpath("//span[@aria-label='Purpose']/text()").get()
+        author['type']=response.xpath("//span[@aria-label='Type']/text()").get()
+        author['added_on']=response.xpath("//span[@aria-label='Reactivated date']/text()").get()
+        author["furnishing"]=response.xpath("//span[@aria-label='Furnishing']/text()").get()
         price={"currency": None,"amount":None}
-        price['currency']=response.xpath("//span[@aria-label='Currency']/text()").extract()
-        price['amount']=response.xpath("//span[@aria-label='Price']/text()").extract()
+        price['currency']=response.xpath("//span[@aria-label='Currency']/text()").get()
+        price['amount']=response.xpath("//span[@aria-label='Price']/text()").get()
         author['price']=price
-        author['location']=response.xpath("//div[@aria-label='Property header']/text()").extract()
+        author['location']=response.xpath("//div[@aria-label='Property header']/text()").get()
         bed_bath_size={"bedrooms":None,"bathrooms":None,"size":None }
-        bed_bath_size['bedrooms']=response.xpath("//span[@class='fc2d1086']/text()").extract()[0]
-        bed_bath_size['bathroom']=response.xpath("//span[@class='fc2d1086']/text()").extract()[1]
-        bed_bath_size['size']=response.xpath("//span[@class='fc2d1086']//span/text()").extract()
+        bed_bath_size['bedrooms']=response.xpath("//span[@class='fc2d1086']/text()").get()
+        bed_bath_size['bathrooms']=response.xpath("//span[@class='fc2d1086']/text()").getall()[1]
+        bed_bath_size['size']=response.xpath("//span[@class='fc2d1086']//span/text()").get()
         author['bed_bath_size']=bed_bath_size
-        author["permit_number"]=response.xpath("//div[@class='_74093213']//span/text()").extract()[-1]
-        author["agent_name"]=response.xpath("//span[@class='_55e4cba0']/text()").extract()[0]
-        author["image_url"]=response.xpath("//img[@src][@aria-label='Cover photo']/@src").extract()
-        author["breadcrumbs"]=">".join(response.xpath("//span[@class='_327a3afc']/text()").extract()[1:])
+        author["permit_number"]=response.xpath("//div[@class='_74093213']//span/text()").getall()[-1]
+        author["agent_name"]=response.xpath("//span[@class='_55e4cba0']/text()").get()
+        author["image_url"]=response.xpath("//img[@src][@aria-label='Cover photo']/@src").get()
+        author["breadcrumbs"]=">".join(response.xpath("//span[@class='_327a3afc']/text()").getall()[1:])
         author["amenities"]=response.xpath("//span[@class='_005a682a']/text()").extract()
-        author["description"]=response.xpath("//span[@class='_2a806e1e']/text()").extract()
+        author["description"]=" ".join(response.xpath("//span[@class='_2a806e1e']/text()").getall())
 
         yield author
 #
+
