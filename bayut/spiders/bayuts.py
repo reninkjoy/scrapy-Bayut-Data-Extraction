2bdb36a91ce4c46a05474a7dcdecc16020edbbca
reninkjoy <44253152+reninkjoy@users.noreply.github.com>
2021-12-22 09:01:35 +0530

    Update bayuts.py
    fetch link

--- bayut/spiders/bayuts.py
+++ bayut/spiders/bayuts.py
@@ -7,4 +7,5 @@ class BayutsSpider(scrapy.Spider):
     start_urls = ['http://https://www.bayut.com/to-rent/property/dubai//']
 
     def parse(self, response):
-        pass
+        for link in response.css('div._4041eb80'):
+            yied{link.css('a._287661cb').attrib['href']}
