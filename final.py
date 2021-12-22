c77a5bca2d702668b95365de004562a906a84af1
reninkjoy <44253152+reninkjoy@users.noreply.github.com>
2021-12-23 01:01:47 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 import json
 
-with open('scrapy-Bayut-Data-Extraction-main/a.json') as f:
+with open('output.json') as f:
   data = json.load(f)
   val=json.dumps(data)
   with open("output.json", "w") as outfile:
